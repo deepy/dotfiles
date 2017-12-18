c134bcf19443f2360879be981d3aa2e86e53b0bb
Alex Nordlund <deep.alexander@gmail.com>
2017-12-18 09:26:32 +0100

    Update xmonad to 0.13

--- xmonad.hs
+++ xmonad.hs
@@ -8,7 +8,7 @@ import System.IO
 
 main = do
     xmproc <- spawnPipe "/home/alex/.cabal/bin/xmobar /home/alex/.xmobarrc"
-    xmonad $ defaultConfig
+    xmonad $ docks def
         { manageHook = manageDocks <+> manageHook defaultConfig
         , layoutHook = avoidStruts  $  layoutHook defaultConfig
         , logHook = dynamicLogWithPP xmobarPP
@@ -19,7 +19,12 @@ main = do
         , startupHook = setWMName "LG3D"
         } `additionalKeys`
         [ ((mod4Mask .|. shiftMask, xK_z), spawn "i3lock")
+        , ((mod4Mask .|. shiftMask .|. controlMask, xK_Return), spawn "/home/alex/.local/bin/alacritty")
         , ((mod4Mask .|. shiftMask, xK_q), kill)
         , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
         , ((0, xK_Print), spawn "scrot")
         ]
+
+myManageHook = composeAll
+    [ className =? "xfce4-notifyd" --> doIgnore
+    ]
