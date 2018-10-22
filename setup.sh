3805f1aaa26a68da6f70ad73493ec2dc09ca78b6
Alex Nordlund <deep.alexander@gmail.com>
2018-10-22 13:45:12 +0200

    Renaming zshrc to assist editors.

--- setup.sh
+++ setup.sh
@@ -3,4 +3,4 @@ cd ~
 git clone https://github.com/zsh-users/antigen.git
 git clone https://github.com/deepy/dotfiles.git
 touch local.zsh
-ln -s dotfiles/zshrc .zshrc
+ln -s dotfiles/zshrc.zsh .zshrc
