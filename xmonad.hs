9ed03db16935653e301b446f0b86f9e72e05d138
Alex Nordlund <deep.alexander@gmail.com>
2019-01-10 10:46:40 +0100

    Adding media keys to xmonad.

--- xmonad.hs
+++ xmonad.hs
@@ -7,7 +7,7 @@ import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 
 main = do
-    xmproc <- spawnPipe "/home/alex/.cabal/bin/xmobar /home/alex/.xmobarrc"
+    xmproc <- spawnPipe "xmobar"
     xmonad $ docks def
         { manageHook = manageDocks <+> manageHook defaultConfig
         , layoutHook = avoidStruts  $  layoutHook defaultConfig
@@ -19,6 +19,10 @@ main = do
         , startupHook = setWMName "LG3D"
         } `additionalKeys`
         [ ((mod4Mask .|. shiftMask, xK_z), spawn "i3lock")
+        , ((0, 0x1008ff12), spawn "amixer -q set Master toggle")
+        , ((0, 0x1008ffb2), spawn "amixer -q set Capture toggle")
+        , ((0, 0x1008ff11), spawn "amixer -q set Master 1- unmute")
+        , ((0, 0x1008ff13), spawn "amixer -q set Master 1+ unmute")
         , ((mod4Mask .|. shiftMask .|. controlMask, xK_Return), spawn "/home/alex/.local/bin/alacritty")
         , ((mod4Mask .|. shiftMask, xK_q), kill)
         , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
