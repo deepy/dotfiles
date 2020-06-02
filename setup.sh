1895fa6b58b4911a6bedf4954b80d2bcc20347c6
Alex Nordlund <deep.alexander@gmail.com>
2020-06-02 17:36:22 +0200

    Add neovimrc

--- setup.sh
+++ setup.sh
@@ -6,4 +6,6 @@ touch local.zsh
 ln -s dotfiles/zshrc.zsh .zshrc
 mkdir -p  ~/.vim/bundle
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+mkdir -p ~/.config/nvim/
+ln -s ~/dotfiles/vimrc ~/.config/nvim/init.vim
 
