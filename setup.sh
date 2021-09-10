500fa14760c7b6d5ad1040082879797c1c6b9b1a
Alex Nordlund <alexander.nordlund@nasdaq.com>
2021-09-10 09:43:23 +0200

    Add Brewfile

--- setup.sh
+++ setup.sh
@@ -8,4 +8,8 @@ mkdir -p  ~/.vim/bundle
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 mkdir -p ~/.config/nvim/
 ln -s ~/dotfiles/vimrc ~/.config/nvim/init.vim
+if [[ `uname` == 'Darwin' ]]; then
+    CURRENT_OS='OS X'
+    ln -s ~/.Brewfile ~/dotfiles/Brewfile
+fi
 
